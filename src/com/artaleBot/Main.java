f43aac305d0f895e3aa83798eaf8d15ca7d6dea5
Jungmin Moon <jmmoon96@gmail.com>
2025-11-03 03:50:21 -0500

    Command validation

--- src/com/artaleBot/Main.java
+++ src/com/artaleBot/Main.java
@@ -2,19 +2,34 @@ package com.artaleBot;
 
 import java.util.Scanner;
 
-public class Main {
+import com.artaleBot.service.CommandCheckService;
 
+public class Main {
+	
 	public static void main(String[] args) {
 		
+		CommandCheckService commandCheckService = new CommandCheckService();
 		
 		try (Scanner input = new Scanner(System.in)) {
 			
 			System.out.println("Welcome to the easy user interface for querying the Artale Database.");
 			System.out.println("Enter a command to begin.");
 			System.out.println("Enter !help to get a list of commands and what they do.");
+			
 			String userInput = input.nextLine();
 			
-			
+			while (!userInput.equalsIgnoreCase("!end")) {
+				
+				if (!commandCheckService.validateCommand(userInput)) {
+					
+					System.out.println("That is not a valid command. Type !help for a list of commands.");
+					
+				} else {
+					commandCheckService.checkCommandType(userInput);
+				}
+				
+				userInput = input.nextLine();
+			}
 			
 		}
 		
--- src/com/artaleBot/service/CommandCheckService.java
+++ src/com/artaleBot/service/CommandCheckService.java
@@ -2,11 +2,67 @@ package com.artaleBot.service;
 
 public class CommandCheckService {
 
-	private void checkCommand(String command) {
+	public void checkCommandType(String command) {
 		
-		switch(command) {
-			
+		String noExclamation = command.substring(1).toLowerCase();
+		
+		
+		switch(noExclamation) {
+			case "mob" -> mobCommand(noExclamation.substring(4));
+			case "boss" -> bossCommand(noExclamation.substring(5));
+			case "equipment" -> equipmentCommand(noExclamation.substring(10));
+			case "mobdrop" -> mobDropCommand(noExclamation.substring(8));
+			case "bossdrop" -> bossDropCommand(noExclamation.substring(9));
+			case "help" -> helpCommand();
 		}
 		
+	}
+	
+	public boolean validateCommand(String command) {
+		boolean validCommand = false;
+		
+		String commandNoExclamationPoint = command.substring(1).toLowerCase();
+		
+		switch (commandNoExclamationPoint) {
+			case "mob" -> validCommand = true;
+			case "boss" -> validCommand = true;
+			case "equipment" -> validCommand = true;
+			case "mobdrop" -> validCommand = true;
+			case "bossdrop" -> validCommand = true;
+			case "help" -> validCommand = true;
+		}
+		
+		return validCommand;
+	}
+	
+	private void mobCommand(String mob) {
+		
+	}
+	
+	private void bossCommand(String boss) {
+		
+	}
+	
+	private void equipmentCommand(String equipment) {
+		
+	}
+	
+	private void mobDropCommand(String mobDrop) {
+		
+	}
+	
+	private void bossDropCommand(String bossDrop) {
+		
+	}
+	
+	private void helpCommand() {
+		System.out.println("The available commands are: ");
+		System.out.println("Note: spelling matters for the search item after the command and a space should be between the command and the search item like in the examples.");
+		System.out.println("!mob: gets information about a mob. Example: !mob Wild Kargo");
+		System.out.println("!boss: gets information about a boss. Example: !boss Mano");
+		System.out.println("!equipment: gets information about a piece of equipment. Example: !equipment Shinkita");
+		System.out.println("!mobdrop: gets information about what mob drops the equipment you are looking for. Example: !mobdrop Blue Ice Queen Skirt");
+		System.out.println("!bossdrop: gets information about what boss drops the equipment you are looking for. Example: !bossdrop Cromi");
+		System.out.println("!help: lists the available commands.");
 	}
 }
