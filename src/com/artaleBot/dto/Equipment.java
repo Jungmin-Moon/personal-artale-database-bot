2f2cf63d2020bbad74b9765b27bb299673c4f1f2
Jungmin Moon <jmmoon96@gmail.com>
2025-11-18 13:26:33 -0500

    Equipment record created

--- src/com/artaleBot/dto/Equipment.java
+++ src/com/artaleBot/dto/Equipment.java
@@ -1,5 +1,8 @@
 package com.artaleBot.dto;
 
-public record Equipment() {
+public record Equipment(String equipmentName, String equipmentType, int minimumLevel, String weaponAtt, String magicAtt, String attSpeed, String weaponDef, String magicDef,
+						int warrior, int magician, int thief, int pirate, int archer, int common, int beginner, int requiredStr, int requiredDex, int requiredInt, int requiredLuk) {
 
+	
+	
 }
